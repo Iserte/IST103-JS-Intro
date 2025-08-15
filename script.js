c218bf2f1f99f8c0fe9b798025b965eca5c31854
Iserte <iserte@bisonic.io>
2025-08-14 19:34:15 -0700

    Log the variables

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const person_name = "Gustavo";
 const person_age = 30;
 const is_student = true;
 const owned_houses = null;
+
+console.log(`Person name is: ${person_name}`);
+console.log(`Person age is: ${person_age}`);
+console.log(`Person is an student? ${is_student}`);
+console.log(`Person has ${owned_houses} houses`);
