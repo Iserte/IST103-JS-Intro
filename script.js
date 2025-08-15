b6caf3f04bd6ab196378e43e46f372dc64ae2db9
Iserte <iserte@bisonic.io>
2025-08-14 19:39:53 -0700

    Add todo_list array

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ console.log(`Person name is: ${person_name}`);
 console.log(`Person age is: ${person_age}`);
 console.log(`Person is an student? ${is_student}`);
 console.log(`Person has ${owned_houses} houses`);
+
+const todo_list = [
+  "Graduate at College",
+  "Get a great job",
+  "Get rich",
+  "Travel a lot",
+];
